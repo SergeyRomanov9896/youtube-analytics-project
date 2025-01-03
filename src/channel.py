f37cfe21177b41817ea4d0fa25d4ac369740b076
SergeyRomanov9896 <automag2016@mail.ru>
2025-01-03 16:15:00 +0300

    Adding documentation

--- src/channel.py
+++ src/channel.py
@@ -4,14 +4,32 @@ import os
 from googleapiclient.discovery import build
 
 class Channel:
-    """Класс для ютуб-канала"""
-    api_key = os.getenv('API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    """
+    Класс для работы с каналом YouTube.
+
+    Attributes:
+        API_KEY (str): Ключ YouTube API, полученный из среды.
+        YOUTUBE: Объект службы API YouTube.
+    """
+    API_KEY = os.getenv('API_KEY')
+    YOUTUBE = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Инициализирует экземпляр канала и получает данные о нем из API.
+
+        :param self.__channel_id: Уникальный идентификатор канала YouTube
+
+        Извлеченная информация сохраняется в следующих атрибутах объекта:
+            self.title (str): Название канала.
+            self.description (str): Описание канала.
+            self.url (str): Ссылка на канал.
+            self.subscriberCount (str): Количество подписчиков.
+            self.video_count (str): Количество видео.
+            self.viewCount (str): Количество просмотров.
+        """
         self.__channel_id = channel_id
-        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        channel = self.YOUTUBE.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
         self.title = ''.join([x['snippet']['title'] for x in channel['items']])
         self.description = ''.join([x['snippet']['description'] for x in channel['items']])
@@ -20,7 +38,12 @@ class Channel:
         self.video_count = ''.join([x['statistics']['videoCount'] for x in channel['items']])
         self.viewCount = ''.join([x['statistics']['viewCount'] for x in channel['items']])
 
+    def __repr__(self):
+        """Возвращает строковое представление объекта для разработчиков."""
+        return f"Channel(id={self.__channel_id}"
+
     def __str__(self) -> str:
+        """Возвращает строковое представление объекта для пользователей."""
         return f"{self.title} ({self.url})"
 
     @property
@@ -30,17 +53,26 @@ class Channel:
 
     @channel_id.setter
     def channel_id(self, new_id: str) -> None:
-        """Не дает изменять id канала."""
+        """
+        Не дает изменять id канала.
+
+        :param new_id: Новый уникальный идентификатор канала YouTube
+        :raises ValueError: Если попытается изменить id канала, вызывается исключение ValueError.
+        """
         if self.__channel_id == new_id:
             raise ValueError("AttributeError: property 'channel_id' of 'Channel' object has no setter")
 
     @classmethod
     def get_service(cls):
-        """Возвращает объект для работы с API."""
-        return cls.youtube
+        """Возвращает объект службы API YouTube."""
+        return cls.YOUTUBE
 
     def to_json(self, path: str) -> None:
-        """Сохраняет данные о канале в json-файл."""
+        """
+        Сохраняет данные о канале в json-файл.
+
+        :param path: Путь к файлу, куда сохранять данные.
+        """
         data = {"id": self.__channel_id,
                  "title": self.title,
                  "description": self.description,
@@ -54,28 +86,30 @@ class Channel:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
     def __add__(self, other) -> int:
+        """Возвращает сумму подписчиков двух каналов."""
         return int(self.subscriberCount) + int(other.subscriberCount)
 
     def __sub__(self, other) -> int:
+        """Возвращает разницу подписчиков двух каналов."""
         return int(self.subscriberCount) - int(other.subscriberCount)
 
     def __gt__(self, other) -> bool:
+        """Возвращает True, если подписчики первого канала больше второго, иначе False."""
         return int(self.subscriberCount) > int(other.subscriberCount)
 
     def __ge__(self, other) -> bool:
+        """Возвращает True, если подписчики первого канала не меньше второго, иначе False."""
         return int(self.subscriberCount) >= int(other.subscriberCount)
 
     def __lt__(self, other) -> bool:
+        """Возвращает True, если подписчики первого канала меньше второго, иначе False."""
         return int(self.subscriberCount) < int(other.subscriberCount)
 
     def __le__(self, other) -> bool:
+        """Возвращает True, если подписчики первого канала не меньше второго, иначе False."""
         return int(self.subscriberCount) <= int(other.subscriberCount)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        def printj(dict_to_print: dict) -> None:
-            """Выводит словарь в json-подобном удобном формате с отступами"""
-            print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        channel = self.YOUTUBE.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
--- src/video.py
+++ src/video.py
@@ -3,41 +3,63 @@ import os
 from googleapiclient.discovery import build
 
 class Video:
-    """Класс для работы с видео."""
+    """
+    Класс для работы с каналом YouTube.
+
+    Attributes:
+        API_KEY (str): Ключ YouTube API, полученный из среды.
+        YOUTUBE: Объект службы API YouTube.
+    """
     API_KEY = os.getenv('API_KEY')
-    youtube = build('youtube', 'v3', developerKey=API_KEY)
-    def __init__(self, video_id):
+    YOUTUBE = build('youtube', 'v3', developerKey=API_KEY)
+
+    def __init__(self, video_id: str) -> None:
         """
-        Инициализирует объект видео.
-        :param video_id: str - id видео
-        :param title: str - заголовок видео
-        :param url: str - ссылка на видео
-        :param view_count: int - количество просмотров видео
-        :param like_count: int - количество лайков видео
+        Извлекает информацию о видео с помощью YouTube Data API v3
+
+        :param video_id: id видео
+
+        Извлеченная информация сохраняется в следующих атрибутах объекта:
+            self.title (str): заголовок видео
+            self.url (str): ссылка на видео
+            self.view_count (str): количество просмотров видео
+            self.like_count (str): количество лайков видео
         """
         self.video_id = video_id
-        video_response = self.youtube.videos().list(part='snippet,statistics', id=self.video_id).execute()
+        video_response = self.YOUTUBE.videos().list(part='snippet,statistics', id=self.video_id).execute()
 
         self.title = ''.join([title['snippet']['title'] for title in video_response['items']])
         self.url = f'https://www.youtube.com/watch?v={self.video_id}'
         self.view_count = ''.join([viewCount['statistics']['viewCount'] for viewCount in video_response['items']])
         self.like_count = ''.join([likeCount['statistics']['likeCount'] for likeCount in video_response['items']])
 
+    def __repr__(self):
+        """Возвращает строковое представление объекта для разработчиков."""
+        return f"Video(id={self.video_id}"
+
     def __str__(self):
+        """Возвращает строковое представление объекта для пользователей."""
         return f"{self.title}"
 
 class PLVideo(Video):
     """Класс для работы с видео из плейлиста."""
-    def __init__(self, video_id, playlist_id):
+    def __init__(self, video_id: str, playlist_id: str) -> None:
         """
-        :param video_id: str - id видео
-        :param playlist_id: str - id плейлиста
+        Инициализирует объект видео из плейлиста.
+
+        :param video_id: - id видео
+        :param playlist_id: - id плейлиста
         """
         super().__init__(video_id)
         self.video_id = video_id
         self.playlist_id = playlist_id
 
+    def __repr__(self):
+        """Возвращает строковое представление объекта для разработчиков."""
+        return f"PLVideo(id={self.video_id}, title={self.playlist_id})"
+
     def __str__(self):
+        """Возвращает строковое представление объекта для пользователей."""
         return f"{self.title}"
 
 
